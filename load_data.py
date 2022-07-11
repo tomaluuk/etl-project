adf5fd379c5d2d50d267998d9f883aaff3ac8513
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-11 23:38:50 +0300

    add function for loading parquet data

--- load_data.py
+++ load_data.py
@@ -16,12 +16,22 @@ def load_jsonl_gz_data(path: str):
     return pd.read_json(path, lines=True, compression='gzip')
 
 
+def load_parquet_data(path: str):
+    """
+    Load parquet data
+    :param str path: Path to file
+    :return DataFrame object of the data: 
+    """
+
+    return ParquetFile(path).to_pandas()
+
+
 df_first_names = load_jsonl_gz_data(
     'data/most-popular-first-names-by-municipality.jsonl.gz')
 
 print(df_first_names.head)
 
-pf_municipalities = ParquetFile('data/municipality-listing-2020-10-21.parquet')
-df_municipalities = pf_municipalities.to_pandas()
+df_municipalities = load_parquet_data(
+    'data/municipality-listing-2020-10-21.parquet')
 
 print(df_municipalities.head())
