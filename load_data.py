9b5b632d7c65d7a2ede23478047475949022fe75
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-11 23:28:02 +0300

    load json.gz data

--- load_data.py
+++ load_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from fastparquet import ParquetFile
-import json
 
-# first_names =
+df_first_names = pd.read_json(
+    path_or_buf='data/most-popular-first-names-by-municipality.jsonl.gz',
+    lines=True,
+    compression='gzip'
+)
 
-pf_municipalities = ParquetFile("data/municipality-listing-2020-10-21.parquet")
-# print(pf_municipalities.columns)
+print(df_first_names.head)
 
+pf_municipalities = ParquetFile('data/municipality-listing-2020-10-21.parquet')
 df_municipalities = pf_municipalities.to_pandas()
 
 print(df_municipalities.head())
