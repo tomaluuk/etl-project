77db03c8047c137fe6a010ba9a1f85de643c4e18
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-31 19:42:08 +0300

    Add drop clauses for idempotency

--- airflow/dags/tomaluuk/sql/create_tables.sql
+++ airflow/dags/tomaluuk/sql/create_tables.sql
@@ -1,5 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS population AUTHORIZATION airflow;
 
+DROP TABLE IF EXISTS population.electoral_district;
+DROP TABLE IF EXISTS population.region;
+DROP TABLE IF EXISTS population.municipality;
+DROP TABLE IF EXISTS population.municipality_birth_stats;
+
+
 CREATE TABLE IF NOT EXISTS population.electoral_district
 (
     electoral_district_number integer NOT NULL,
@@ -8,6 +14,7 @@ CREATE TABLE IF NOT EXISTS population.electoral_district
     PRIMARY KEY (electoral_district_number)
 );
 
+
 CREATE TABLE IF NOT EXISTS population.region
 (
     region_number integer NOT NULL,
@@ -15,6 +22,7 @@ CREATE TABLE IF NOT EXISTS population.region
     name_se character varying(255),
     PRIMARY KEY (region_number)
 );
+
 
 CREATE TABLE IF NOT EXISTS population.municipality
 (
@@ -25,25 +33,30 @@ CREATE TABLE IF NOT EXISTS population.municipality
     name_se character varying(255),
     type character varying(255),
     primary_language character varying(255),
-    PRIMARY KEY (municipality_number),
-    CONSTRAINT fk_electoral_district
+    PRIMARY KEY (municipality_number)
+    /*
+    ,CONSTRAINT fk_electoral_district
         FOREIGN KEY (electoral_district_number)
             REFERENCES population.electoral_district(electoral_district_number),
     CONSTRAINT fk_region
         FOREIGN KEY (region_number)
             REFERENCES population.region(region_number)
+    */
 );
 
 
-CREATE TABLE IF NOT EXISTS population.birth_name_stats
+CREATE TABLE IF NOT EXISTS population.municipality_birth_stats
 (
-    birth_name_stats_id integer NOT NULL,
+    id text NOT NULL,
     municipality_number integer NOT NULL,
+    year_of_birth integer NOT NULL,
+    first_name character varying(255) NOT NULL,
     gender boolean,
-    first_name character varying(255),
     amount integer,
-    PRIMARY KEY (birth_name_stats_id),
-    CONSTRAINT fk_municipality
+    PRIMARY KEY (id)
+    /*
+    ,CONSTRAINT fk_municipality
         FOREIGN KEY (municipality_number)
             REFERENCES population.municipality(municipality_number)
+    */
 );
