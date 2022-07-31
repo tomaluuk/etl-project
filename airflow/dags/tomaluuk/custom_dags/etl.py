0f1a47570dfbe8c7fab80bdd75634c2ad01ced33
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-31 18:20:54 +0300

    Add template searchpath to dag, fix tasks, remove test task

--- airflow/dags/tomaluuk/custom_dags/etl.py
+++ airflow/dags/tomaluuk/custom_dags/etl.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from tomaluuk.extract import extract_municipality_popular_names_data
+from tomaluuk.extract import extract_source_data
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -9,7 +9,7 @@ from airflow.decorators import task
 # Operators; we need this to operate!
 #from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
-#from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 #DATA_FILES_PATH = "./data/"
 #DATA_FILES = os.listdir(DATA_FILES_PATH)
@@ -39,44 +39,20 @@ with DAG(
         # 'trigger_rule': 'all_success'
     },
     description='A demo ETL DAG',
+    template_searchpath='/opt/airflow/dags/tomaluuk',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['example'],
 ) as dag:
 
-    # @task.virtualenv(task_id='test', requirements=["fastparquet", "pandas"])
-    extract_municipality_popular_names_data_task = PythonVirtualenvOperator(
-        python_callable=extract_municipality_popular_names_data
+    create_tables_task = PostgresOperator(
+        task_id="create-tables",
+        postgres_conn_id='postgres_airflow_worker',
+        sql="sql/create_tables.sql"
     )
-    # @task.virtualenv(
-    #    # "./requirements.txt"
-    #    task_id='test2', requirements=["colorama==0.4.0"]
-    # )
-    # def callable_virtualenv():  # extract_municipality_popular_names_data_task():
-    #    """
-    #     Example function that will be performed in a virtual environment.
-#
-    #     Importing at the module level ensures that it will not attempt to import the
-    #     library before it is installed.
-    #     """
-    #    from time import sleep
-#
-    #    from colorama import Back, Fore, Style
-#
-    #    print(Fore.RED + 'some red text')
-    #    print(Back.GREEN + 'and with a green background')
-    #    print(Style.DIM + 'and in dim text')
-    #    print(Style.RESET_ALL)
-    #    for _ in range(10):
-    #        print(Style.DIM + 'Please wait...', flush=True)
-    #        sleep(2)
-    #    print('Finished')
-#
-    #virtualenv_task = callable_virtualenv()
-
-    # create_tables_task = PostgresOperator(
-    #    task_id="create_municipality_table",
-    #    postgres_conn_id="sql_qlchemy_conn",
-    #    sql="sql/employees_schema.sql"
-    # )
+    extract_source_data_task = PythonOperator(
+        task_id='extract-source-data',
+        python_callable=extract_source_data.main
+    )
+    create_tables_task >> extract_source_data_task
