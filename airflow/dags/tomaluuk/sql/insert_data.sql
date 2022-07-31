7433ccc2849b04b7c737fd1a61a937d26c012da6
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-31 19:42:52 +0300

    Add municipality_birth stats query to insert_data task

--- airflow/dags/tomaluuk/sql/insert_data.sql
+++ airflow/dags/tomaluuk/sql/insert_data.sql
@@ -1,7 +1,8 @@
 delete from population.electoral_district;
 delete from population.region;
-delete from population.municipality;
- 
+delete from population.municipality; 
+delete from population.municipality_birth_stats; 
+
 
 insert into population.electoral_district
 select distinct
@@ -32,3 +33,19 @@ select distinct
 	primary_language
 from sources.municipality_listing
 where municipality_number is not null;
+
+
+insert into population.municipality_birth_stats
+with stats as (
+select
+	MD5(birth_municipality_number::text || year_of_birth::text || first_name::text) as id,
+	birth_municipality_number as municipality_number,
+	year_of_birth,
+	first_name,
+	gender::int::boolean,
+	amount
+from sources.most_popular_first_names_by_municipality
+)
+select *
+from stats
+where id is not null;
