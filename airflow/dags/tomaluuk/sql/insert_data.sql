b1d472f3e7f92dd4b44343d612f7657328c0da20
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-07-31 19:13:27 +0300

    Add region & municipality queries to insert_data_task

--- airflow/dags/tomaluuk/sql/insert_data.sql
+++ airflow/dags/tomaluuk/sql/insert_data.sql
@@ -1,4 +1,7 @@
 delete from population.electoral_district;
+delete from population.region;
+delete from population.municipality;
+ 
 
 insert into population.electoral_district
 select distinct
@@ -8,3 +11,24 @@ select distinct
 from sources.municipality_listing
 where electoral_district_number is not null;
 
+
+insert into population.region
+select distinct
+  region_number,
+  region_name_fi,
+  region_name_se
+from sources.municipality_listing
+where region_number is not null;
+
+
+insert into population.municipality 
+select distinct
+	municipality_number,
+	electoral_district_number,
+	region_number,
+	municipality_name_fi as name_fi,
+	municipality_name_se as name_se,
+	municipality_type as type,
+	primary_language
+from sources.municipality_listing
+where municipality_number is not null;
