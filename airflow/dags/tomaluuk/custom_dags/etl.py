06e641a9e63a6f42792ead582809b62d7913c1df
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2022-08-03 22:41:42 +0300

    Remove unused code

--- airflow/dags/tomaluuk/custom_dags/etl.py
+++ airflow/dags/tomaluuk/custom_dags/etl.py
@@ -2,22 +2,13 @@ import os
 from datetime import datetime, timedelta
 from tomaluuk.extract import extract_source_data
 
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-from airflow.decorators import task
 
-# Operators; we need this to operate!
-#from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
-#DATA_FILES_PATH = "./data/"
-#DATA_FILES = os.listdir(DATA_FILES_PATH)
 
 with DAG(
     'demo-etl',
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
     default_args={
         'depends_on_past': False,
         'email': ['luukkane@gmail.com'],
@@ -25,18 +16,6 @@ with DAG(
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function,
-        # 'on_success_callback': some_other_function,
-        # 'on_retry_callback': another_function,
-        # 'sla_miss_callback': yet_another_function,
-        # 'trigger_rule': 'all_success'
     },
     description='A demo ETL DAG',
     template_searchpath='/opt/airflow/dags/tomaluuk',
